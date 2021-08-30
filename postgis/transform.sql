e4f419a4f909deaf7742b74dab678496b490ad5e
JK <uzmystic@gmail.com>
2021-08-30 19:26:33 +0900

    Create transform.sql

---
+++ postgis/transform.sql
@@ -0,0 +1,15 @@
+insert
+	into
+	map_source.fragment
+(
+	geometry,
+	fragment_id,
+	id,
+	"name"
+)
+values(
+  ST_Transform (ST_GeomFromText('POINT(129.1911187 36.20630204)', 4326), 3857),
+  6,
+  '',
+  'hello world'
+);
